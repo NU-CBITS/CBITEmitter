e7ce59b126f953378ca9c7444a59c357b5dc64ab
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-08-24 16:21:01 -0500

    refactored with private methods

--- src/ios/CBITEmitter.m
+++ src/ios/CBITEmitter.m
@@ -5,56 +5,76 @@
 
 @implementation CBITEmitter
 
-- (void)emitData:(CDVInvokedUrlCommand*)command
+- (void)emitData:(CDVInvokedUrlCommand *)command
 {
-  NSString* serverURI = [command.arguments objectAtIndex:0];
-  NSString* json = [command.arguments objectAtIndex:1];
-  NSLog(@"%@", json);
-  NSURL* url = [NSURL URLWithString:serverURI];
-
-  // Convert data and store in a file.
+  NSString *serverURI = [command.arguments objectAtIndex:0];
+  NSString *json = [command.arguments objectAtIndex:1];
+  NSLog(@"emitData called with payload: %@", json);
+  NSURL *url = [NSURL URLWithString:serverURI];
   NSData *requestData = [NSData dataWithBytes:[json UTF8String]
                                        length:[json length]];
-  NSArray *dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-  NSString *docsDir = [dirPaths objectAtIndex:0];
-  NSURL *dataURL = [[NSURL alloc] initFileURLWithPath: [docsDir stringByAppendingPathComponent:@"cbit.data"]];
+  NSURL *dataURL = [self dataPath];
 
   if ([requestData writeToURL:dataURL atomically:YES]) {
-    NSLog(@"%@", dataURL);
-    NSMutableURLRequest* request = [[NSMutableURLRequest alloc] initWithURL:url];
-    [request setHTTPMethod:@"POST"];
-    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    [request setValue:[NSString stringWithFormat:@"%d", [requestData length]] forHTTPHeaderField:@"Content-Length"];
-
-    // Make a background session and kick it off.
-    NSURLSessionConfiguration *backgroundConfiguration = [NSURLSessionConfiguration backgroundSessionConfiguration:@"edu.northwestern.cbits.cbitemitter"];
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:backgroundConfiguration
-                                                          delegate:(id)self
-                                                     delegateQueue:nil];
-    NSURLSessionTask *task = [session uploadTaskWithRequest:request
-                                                   fromFile:dataURL];
+    NSLog(@"POSTING to URL: %@", dataURL);
+    NSMutableURLRequest *request = [self formRequest:url
+                                            withData:requestData];
+    NSURLSessionTask *task = [self createBackgroundUrlSessionTask:request
+                                                     fromDataFile:dataURL];
     [task resume];
   }
   else {
-    NSLog(@"%@", @"not written!");
+    NSLog(@"%@", @"request data file not written!");
   }
 }
+
+/* Session delegate methods */
 
 - (void)URLSession:(NSURLSession *)session
           dataTask:(NSURLSessionDataTask *)dataTask
     didReceiveData:(NSData *)data
 {
-    //handle data here
-    NSLog(@"%@", @"data");
+    NSLog(@"%@", @"data received");
 }
 
 - (void)URLSession:(NSURLSession *)session
                     task:(NSURLSessionTask *)task
     didCompleteWithError:(NSError *)error
 {
-  //Called when the data transfer is complete
-  //Client side errors are indicated with the error parameter
-  NSLog(@"%@", @"complete");
+  NSLog(@"%@", @"request complete");
+}
+
+/* Private methods */
+
+- (NSURL *)dataPath
+{
+  NSArray *dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+  NSString *docsDir = [dirPaths objectAtIndex:0];
+
+  return [[NSURL alloc] initFileURLWithPath: [docsDir stringByAppendingPathComponent:@"cbit.data"]];
+}
+
+- (NSMutableURLRequest *)formRequest:(NSURL *)url
+                            withData:(NSData *)data
+{
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request setValue:[NSString stringWithFormat:@"%d", [data length]] forHTTPHeaderField:@"Content-Length"];
+
+    return request;
+}
+
+- (NSURLSessionTask *)createBackgroundUrlSessionTask:(NSMutableURLRequest *)request
+                                        fromDataFile:(NSURL *)dataURL
+{
+    NSURLSessionConfiguration *backgroundConfiguration = [NSURLSessionConfiguration backgroundSessionConfiguration:@"edu.northwestern.cbits.cbitemitter"];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:backgroundConfiguration
+                                                          delegate:(id)self
+                                                     delegateQueue:nil];
+
+    return [session uploadTaskWithRequest:request
+                                 fromFile:dataURL];
 }
 @end
